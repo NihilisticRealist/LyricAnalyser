ffbd49f0dbe0c7f13c5801441e398b78ed075208
Thomas Plans <thomas@khandaan.org>
2018-10-11 18:54:42 +0100

    Made slightly easier to read and edit.

--- LyricAnalyser.py
+++ LyricAnalyser.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
+
 words = {}
+
+#    EDIT THIS LINE FOR OTHER FILES
 with open("inMind.txt", "r") as song:
+
     for lyric in song:
         for word in lyric.lower().split(" "):
             word = word.strip("\n").strip(",").strip(".").strip(";").strip(":").strip("?").strip("!").strip("'").strip('"')
@@ -9,4 +13,5 @@ with open("inMind.txt", "r") as song:
 
             else:
                 words[word] = words[word] + 1
+
 plt.bar(sorted(words, key=words.__getitem__, reverse=True), sorted(words.values(), reverse=True), 1, color='b'); plt.show()
