072f7ca5e4b9bf3a49d17715bfe3ffb55cd0a404
Thomas Plans <thomas@khandaan.org>
2018-10-11 18:44:52 +0100

    Created the basic system.

---
+++ LyricAnalyser.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+words = {}
+with open("inMind.txt", "r") as song:
+    for lyric in song:
+        for word in lyric.lower().split(" "):
+            word = word.strip("\n").strip(",").strip(".").strip(";").strip(":").strip("?").strip("!").strip("'").strip('"')
+            if word not in words:
+                words[word] = 1
+
+            else:
+                words[word] = words[word] + 1
+plt.bar(sorted(words, key=words.__getitem__, reverse=True), sorted(words.values(), reverse=True), 1, color='b'); plt.show()
